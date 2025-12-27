195cc972d845e776bb77300759902d59f58dac05
TManish-3D <mtimsina91@gmail.com>
2025-12-27 20:24:40 +0545

    with flask-app

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template ("main.html")
+@app.route('/about')
+def about():
+    return render_template ("main.html")
+@app.route('/skills')
+def skills():
+    return render_template ("skills.html")
+@app.route('/projects')
+def projects():
+    return render_template ("projects.html")
+
+if __name__ == '__main__':
+     app.run(debug=True)
